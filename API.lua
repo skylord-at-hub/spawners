c26389c8c10854c9b540c6c0d683444e2cf46183
Juraj Vajda <juraj.vajda@gmail.com>
2016-01-14 00:18:37 +0100

    big cleanup and tweaks

---
+++ API.lua
@@ -0,0 +1,169 @@
+-- main tables
+spawners = {}
+spawners.mob_tables = {}
+
+-- check if mods exists and build tables
+for k, mob_mod in ipairs(ENABLED_MODS) do
+	local modpath = minetest.get_modpath(mob_mod)
+	-- list of mobs and their info
+	if (modpath) then
+		for j, mob in ipairs(MOBS_PROPS[mob_mod]) do
+			local mob_egg = nil
+
+			if mob_mod == "mobs" and not (mobs.mod == "redo") then goto continue end
+
+			table.insert(spawners.mob_tables, {name=mob.name, mod_prefix=mob_mod, egg_name_custom=mob.egg_name_custom, dummy_size=mob.dummy_size, dummy_offset=mob.dummy_offset, dummy_mesh=mob.dummy_mesh, dummy_texture=mob.dummy_texture, night_only=mob.night_only, sound_custom=mob.sound_custom})
+
+			-- use custom egg or create a default egg
+			if mob.egg_name_custom ~= "" then 
+				mob_egg = mob.egg_name_custom
+			else
+				mob_egg = mob_mod..":"..mob.name
+			end
+			
+			-- recipes
+			minetest.register_craft({
+				output = "spawners:"..mob_mod.."_"..mob.name.."_spawner",
+				recipe = {
+					{"default:diamondblock", "fake_fire:flint_and_steel", "default:diamondblock"},
+					{"xpanes:bar", mob_egg, "xpanes:bar"},
+					{"default:diamondblock", "xpanes:bar", "default:diamondblock"},
+				}
+			})
+
+			::continue::
+		end
+	else
+		print("[MOD] Spawners: MOD "..mob_mod.." not found.")
+	end
+end
+
+-- start spawning mobs
+function spawners.start_spawning(pos, how_many, mob_name, mod_prefix, sound_custom)
+	if not pos or not how_many or not mob_name then return end
+
+	local sound_name
+	-- remove 'spawners:' from the string
+	local mob_name = string.sub(mob_name,10)
+
+	-- use custom sounds
+	if sound_custom ~= "" then 
+		sound_name = sound_custom
+	else
+		sound_name = mod_prefix.."_"..mob_name
+	end
+
+	for i=1,how_many do
+		local obj = minetest.add_entity(pos, mod_prefix..":"..mob_name)
+
+		if obj then
+
+			if sound_name then
+				minetest.sound_play(sound_name, {
+					pos = pos,
+					max_hear_distance = 32,
+					gain = 10,
+				})
+			end
+
+		end
+	end
+	
+end
+
+function spawners.check_around_radius(pos)
+	local player_near = false
+	local radius = 21
+
+	for _,obj in ipairs(minetest.get_objects_inside_radius(pos, radius)) do
+		if obj:is_player() then
+			player_near = true
+		end
+	end
+
+	return player_near
+end
+
+function spawners.check_node_status(pos, mob, night_only)
+	local player_near = spawners.check_around_radius(pos)
+	local random_pos = false
+
+	if player_near then
+		local min_node_light = 10
+		local tod = minetest.get_timeofday() * 24000
+		local node_light = minetest.get_node_light(pos)
+		local spawn_positions = {}
+		local right = minetest.get_node({x=pos.x+1, y=pos.y, z=pos.z})
+		local front = minetest.get_node({x=pos.x, y=pos.y, z=pos.z+1})
+		local left = minetest.get_node({x=pos.x-1, y=pos.y, z=pos.z})
+		local back = minetest.get_node({x=pos.x, y=pos.y, z=pos.z-1})
+		local top = minetest.get_node({x=pos.x, y=pos.y+1, z=pos.z})
+		local bottom = minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z})
+
+		if not node_light then return false end
+		
+		-- make sure that at least one side of the spawner is open
+		if right.name == "air" then
+			table.insert(spawn_positions, {x=pos.x+1.5, y=pos.y, z=pos.z})
+		end
+		if front.name == "air" then
+			table.insert(spawn_positions, {x=pos.x, y=pos.y, z=pos.z+1.5})
+		end
+		if left.name == "air" then
+			table.insert(spawn_positions, {x=pos.x-1.5, y=pos.y, z=pos.z})
+		end
+		if back.name == "air" then
+			table.insert(spawn_positions, {x=pos.x, y=pos.y, z=pos.z-1.5})
+		end
+		if top.name == "air" then
+			table.insert(spawn_positions, {x=pos.x, y=pos.y+1.5, z=pos.z})
+		end
+		if bottom.name == "air" then
+			table.insert(spawn_positions, {x=pos.x, y=pos.y-1.5, z=pos.z})
+		end
+
+		if #spawn_positions < 1 then
+			-- spawner is closed from all sides
+			return false
+		else
+			-- pick random from the open sides
+			local pick_random
+
+			if #spawn_positions == 1 then
+				pick_random = #spawn_positions
+			else
+				pick_random = math.random(1,#spawn_positions)
+			end
+			
+			for k, v in pairs (spawn_positions) do
+				if k == pick_random then
+					random_pos = v
+				end
+			end
+		end
+
+		-- check the node above and below the found air node
+		local node_above = minetest.get_node({x=random_pos.x, y=random_pos.y+1, z=random_pos.z}).name
+		local node_below = minetest.get_node({x=random_pos.x, y=random_pos.y-1, z=random_pos.z}).name
+		
+		if not (node_above == "air" or node_below == "air") then return false end
+
+		-- spawn only at day
+		if not night_only and node_light < min_node_light then
+			return false, true
+		end
+
+		-- spawn only at night
+		if night_only then
+			if not (19359 > tod and tod > 5200) or node_light < min_node_light then
+				return random_pos
+			else
+				return false, true
+			end
+		end
+
+		return random_pos
+	else
+		return false
+	end
+end
--- config.lua
+++ config.lua
@@ -1,25 +1,144 @@
--- mod what should be enabled and loded
+-- * [name : string] - Name of the mob used in the mod.
+
+-- [egg_name_custom : string] - Custom name for the egg item. If empty default name will be used i.e. 'mobs:chicken'.
+
+-- * [dummy_size : table] - Size of the rotating dummy inside the node.
+
+-- * [dummy_offset : integer] - Offset on Y axis of the dummy inside the node.
+
+-- * [dummy_mesh : string] - Filename of the model used fot he mob.
+
+-- * [dummy_texture : table] - Textures used for the mob.
+
+-- * [night_only : string] - If true mobs will spawn only during the night or in dark areas, default:true.
+
+-- [sound_custom : string] - Custom name for the sound file name if differ from default: i.e 'mobs_cow'.
+
+-- [*] -> MANDATORY - has to be filled in!
+
+-- mods what should be enabled and loded
 ENABLED_MODS = {"mobs", "pyramids", "creatures"}
 
 -- mobs properties - setup all you mobs here
 MOBS_PROPS = {
-	["mobs"] = {
-		{name = "mobs name 1"},	
-		{name = "mobs name 2"},	
-		{name = "mobs name 3"},	
+	["mobs"] = { -- MOBS REDO CONFIG
+		{
+			name="sheep_white",
+			egg_name_custom="",
+			dummy_size={x=0.52,y=0.52},
+			dummy_offset=0.2,
+			dummy_mesh="mobs_sheep.b3d",
+			dummy_texture={"mobs_sheep_white.png"},
+			night_only=false,
+			sound_custom="mobs_sheep"
+		},
+		{
+			name="cow",
+			egg_name_custom="",
+			dummy_size={x=0.3,y=0.3},
+			dummy_offset=-0.3,
+			dummy_mesh="mobs_cow.x",
+			dummy_texture={"mobs_cow.png"},
+			night_only=false,
+			sound_custom=""
+		},
+		{
+			name="chicken",
+			egg_name_custom="",
+			dummy_size={x=0.9,y=0.9},
+			dummy_offset=0.2,
+			dummy_mesh="mobs_chicken.x",
+			dummy_texture={"mobs_chicken.png", "mobs_chicken.png", "mobs_chicken.png", "mobs_chicken.png", "mobs_chicken.png", "mobs_chicken.png", "mobs_chicken.png", "mobs_chicken.png", "mobs_chicken.png"},
+			night_only=false,
+			sound_custom=""
+		},
+		{
+			name="warthog",
+			egg_name_custom="",
+			dummy_size={x=0.62,y=0.62},
+			dummy_offset=-0.3,
+			dummy_mesh="mobs_warthog.x",
+			dummy_texture={"mobs_warthog.png"},
+			night_only=false,
+			sound_custom="mobs_pig"
+		},
+		{
+			name="bunny",
+			egg_name_custom="",
+			dummy_size={x=1,y=1},
+			dummy_offset=0.2,
+			dummy_mesh="mobs_bunny.b3d",
+			dummy_texture={"mobs_bunny_brown.png"},
+			night_only=false,
+			sound_custom="spawners_bunny"
+		},
+		{
+			name="kitten",
+			egg_name_custom="",
+			dummy_size={x=0.32,y=0.32},
+			dummy_offset=0,
+			dummy_mesh="mobs_kitten.b3d",
+			dummy_texture={"mobs_kitten_ginger.png"},
+			night_only=false,
+			sound_custom=""
+		}
 	},
 
-	["pyramids"] = {
-		{name = "pyramids name 1"},	
-		{name = "pyramids name 2"},	
-		{name = "pyramids name 3"}	
+	["pyramids"] = { -- PYRAMIDS MOD CONFIG
+		{
+			name="mummy",
+			egg_name_custom="pyramids:spawn_egg",
+			dummy_size={x=3.3,y=3.3},
+			dummy_offset=-0.3,
+			dummy_mesh="pyramids_mummy.x",
+			dummy_texture={"pyramids_mummy.png"},
+			night_only=false,
+			sound_custom="mummy"
+		}
 	},
 
 
-	["creatures"] = {
-		{name = "creatures name 1"},	
-		{name = "creatures name 2"},	
-		{name = "creatures name 3"}	
+	["creatures"] = { -- CREATURES MOD CONFIG
+		{
+			name="chicken",
+			egg_name_custom="creatures:chicken_spawn_egg",
+			dummy_size={x=0.9,y=0.9},
+			dummy_offset=-0.3,
+			dummy_mesh="creatures_chicken.b3d",
+			dummy_texture={"creatures_chicken.png"},
+			night_only=false,
+			sound_custom=""
+		},	
+		{
+			name="ghost",
+			egg_name_custom="creatures:ghost_spawn_egg",
+			dummy_size={x=0.7,y=0.7},
+			dummy_offset=-0.5,
+			dummy_mesh="creatures_ghost.b3d",
+			dummy_texture={"creatures_ghost.png"},
+			night_only=true,
+			sound_custom=""
+		},	
+		{
+			name="sheep",
+			egg_name_custom="creatures:sheep_spawn_egg",
+			dummy_size={x=0.6,y=0.6},
+			dummy_offset=-0.3,
+			dummy_mesh="creatures_sheep.b3d",
+			dummy_texture={"creatures_sheep.png^creatures_sheep_white.png"},
+			night_only=false,
+			sound_custom=""
+		},	
+		{
+			name="zombie",
+			egg_name_custom="creatures:zombie_spawn_egg",
+			dummy_size={x=0.5,y=0.5},
+			dummy_offset=-0.5,
+			dummy_mesh="creatures_zombie.b3d",
+			dummy_texture={"creatures_zombie.png"},
+			night_only=true,
+			sound_custom=""
+		}	
 	}
 
 }
