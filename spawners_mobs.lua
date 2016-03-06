99ebe7f56077df440f4f428960c52d9d71c27646
juraj <juraj.vajda@gmail.com>
2016-03-06 22:38:04 +0100

    reverting last abm changes

--- spawners_mobs.lua
+++ spawners_mobs.lua
@@ -325,8 +325,8 @@ function spawners.create(mob_name, mod_prefix, size, offset, mesh, texture, nigh
 			"spawners:"..mod_prefix.."_"..mob_name.."_spawner_waiting_env"
 		},
 		neighbors = {"air"},
-		interval = 15.0,
-		chance = 5,
+		interval = 10.0,
+		chance = 6,
 		catch_up = false,
 		action = function(pos, node, active_object_count, active_object_count_wider)
 
