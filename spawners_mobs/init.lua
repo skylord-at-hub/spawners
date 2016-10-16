83838c91bf1f9937935f811187f6f4a45961f6cc
Juraj Vajda <juraj.vajda@rituals.com>
2016-10-16 11:23:36 +0100

    add evil bunny to init file

--- spawners_mobs/init.lua
+++ spawners_mobs/init.lua
@@ -12,6 +12,7 @@ dofile(minetest.get_modpath(MOD_NAME).."/spawners_mobs.lua")
 -- include mummy mobs redo addon (mob)
 if minetest.get_modpath("mobs") then
 	dofile(minetest.get_modpath(MOD_NAME).."/mob_mummy.lua")
+	dofile(minetest.get_modpath(MOD_NAME).."/mob_bunny_evil.lua")
 	dofile(minetest.get_modpath(MOD_NAME).."/nodes_additional.lua")
 end
 
