f0e64265ee7e57457121b13ce7c91f88d5259c53
Juraj Vajda <juraj.vajda@rituals.com>
2016-10-16 15:26:49 +0100

    mob mummy shoots skulls now

--- spawners_mobs/mob_mummy.lua
+++ spawners_mobs/mob_mummy.lua
@@ -3,12 +3,17 @@
 local mummy_def = {
 	type = "monster",
 	passive = false,
-	attack_type = "dogfight",
 	pathfinding = true,
-	reach = 2,
+	attack_type = "dogshoot",
+	shoot_interval = 2,
+	dogshoot_switch = 1,
+	dogshoot_count_max = 10,
+	arrow = "spawners_mobs:black_skull",
+	shoot_offset = 2,
+	reach = 3,
 	damage = 4,
 	hp_min = 25,
-	hp_max = 35,
+	hp_max = 45,
 	armor = 100,
 	collisionbox = {-0.4, -1, -0.4, 0.4, 0.8, 0.4},
 	visual = "mesh",
@@ -20,10 +25,11 @@ local mummy_def = {
 	sounds = {
 		random = "spawners_mobs_mummy",
 		damage = "spawners_mobs_mummy_hit",
+		shoot_attack = "spawners_mobs_mummy_shoot",
 	},
 	walk_velocity = .75,
 	run_velocity = 1.5,
-	view_range = 8,
+	view_range = 15,
 	jump = true,
 	floats = 0,
 	drops = {
@@ -60,6 +66,32 @@ local mummy_def = {
 
 mobs:register_mob("spawners_mobs:mummy", mummy_def)
 
-mobs:register_spawn("spawners_mobs:mummy", {"default:desert_sand", "default:desert_stone"}, 20, 0, 14000, 2, 31000)
+mobs:register_spawn("spawners_mobs:mummy", {"default:desert_sand", "default:desert_stone", "default:sand", "default:sandstone", "default:silver_sand"}, 20, 0, 14000, 2, 31000)
 
 mobs:register_egg("spawners_mobs:mummy", "Mummy Monster", "default_sandstone_brick.png", 1)
+
+-- black skull shooting
+mobs:register_arrow("spawners_mobs:black_skull", {
+	visual = "sprite",
+	visual_size = {x = 1, y = 1},
+	textures = {"spawners_mobs_black_skull.png"},
+	velocity = 7,
+	-- tail = 1,
+	-- tail_texture = "spawners_mobs_black_skull.png",
+	-- tail_size = 10,
+
+	-- direct hit, no fire... just plenty of pain
+	hit_player = function(self, player)
+		player:punch(self.object, 1.0, {
+			full_punch_interval = 1.0,
+			damage_groups = {fleshy = 8},
+		}, nil)
+	end,
+
+	hit_mob = function(self, player)
+		player:punch(self.object, 1.0, {
+			full_punch_interval = 1.0,
+			damage_groups = {fleshy = 8},
+		}, nil)
+	end,
+})
