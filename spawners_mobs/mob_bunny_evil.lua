a5319a4b62eb428b0d9d7bdd3683983da7e0b1ed
Juraj Vajda <juraj.vajda@rituals.com>
2016-11-30 23:52:51 +0100

    lower chance of drops for evil bunny

--- spawners_mobs/mob_bunny_evil.lua
+++ spawners_mobs/mob_bunny_evil.lua
@@ -28,7 +28,7 @@ local bunny_evil_def = {
 	jump = true,
 	floats = 0,
 	drops = {
-		{name = "mobs:meat_raw", chance = 1, min = 1, max = 1},
+		{name = "mobs:meat_raw", chance = 5, min = 1, max = 1},
 		{name = "spawners_mobs:bunny_evil", chance = 40, min = 1, max = 1},
 	},
 	water_damage = 3,
